4d05a79dcd1886c5b222786b6bf4d2a2d5f20f98
GazziFX <43498426+GazziFX@users.noreply.github.com>
2022-10-18 01:25:09 +0300

    Add files via upload

---
+++ Module.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using SDG.Framework.Modules;
+using SDG.Unturned;
+
+namespace ItemCountExpander
+{
+    [HarmonyPatch]
+    public class Module : IModuleNexus
+    {
+        private readonly Harmony HarmonyInstance = new Harmony(nameof(ItemCountExpander));
+
+        public void initialize()
+        {
+            HarmonyInstance.PatchAll(typeof(Module).Assembly);
+        }
+
+        public void shutdown()
+        {
+            HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+        }
+
+        [HarmonyPatch(typeof(PlayerInventory), "ReceiveDragItem")]
+        [HarmonyPatch(typeof(Items), "tryAddItem", typeof(Item), typeof(bool))]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var inst in instructions)
+            {
+                if (inst.opcode == OpCodes.Ldc_I4 && (int)inst.operand == 200)
+                    inst.operand = 255;
+
+                yield return inst;
+            }
+        }
+    }
+}
